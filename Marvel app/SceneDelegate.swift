cc0b178cc4ea5825c6beba9aa6556c1f33eacfad
SalvadorCarnelutti <salvadorcarnelutti@gmail.com>
2023-03-31 20:04:11 -0400

    Added Firebase SDK

--- Marvel app/SceneDelegate.swift
+++ Marvel app/SceneDelegate.swift
@@ -19,7 +19,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ViewController()
+        let viewController = ViewController()
+        window.rootViewController = viewController
         self.window = window
         window.makeKeyAndVisible()
     }
