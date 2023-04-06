e36a84fb97805b209f584c7f507297ef30791294
SalvadorCarnelutti <salvadorcarnelutti@gmail.com>
2023-04-06 16:18:45 -0400

    Removed TODO comments

--- Marvel app/Utilities/CryptoHelper.swift
+++ Marvel app/Utilities/CryptoHelper.swift
@@ -9,7 +9,6 @@ import Foundation
 import CryptoKit
 
 class CryptoHelper {
-    // TODO: Change for cocopods key (Maybe)
     static let publicKey = "5b23e88d26cf56958f076f88be9fed9d"
     static let privateKey = "b220d549848ca21eacd776e73db60e6deccfd95f"
     
